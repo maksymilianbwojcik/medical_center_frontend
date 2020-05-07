88e48975ca77bf3a6f0adb8eb30dca9851111912
Khamzat <khamzat@localhost.localdomain>
2020-05-07 15:06:21 +0200

    adding demo layout

--- medical-center/src/App.js
+++ medical-center/src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  
+class App extends Component{
+    render(){
+        return(
+            <div id="logo" className="container">
+                <h1><span className="icon icon-ambulance icon-size"></span><a href="#">Medi<span>Shield</span></a></h1>
+            </div>
+        );
+    }
 }
 
 export default App;
