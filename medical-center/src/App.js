baecde41ab26e5806484ca0ba10a307392ce86be
Khamzat <khamzat@localhost.localdomain>
2020-06-18 16:52:29 +0200

    generalnie rejestracja juz dziala, logowanie jest w trakcie podpinania i wgl ten commit jest dla mojego brata

--- medical-center/src/App.js
+++ medical-center/src/App.js
@@ -1,59 +1,120 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import Menu from './Menu';
-import Home from './Home';
-import Faq from './Faq';
-import Footer from './Footer';
-import Register from './Register';
-import Login from './Login';
-import Cookies from 'js-cookie'
+import React, { Component } from 'react';
+import './App.css';
+import {
+    Route,
+    withRouter,
+    Switch
+} from 'react-router-dom';
+
+import { getCurrentUser } from './utils/APIUtils';
+import { ACCESS_TOKEN } from './constants';
+
+import Profile from "./Profile";
+import AppHeader from "./component/AppHeader";
+import Login from 'Login';
+import Signup from 'Signup';
 
 
-class App extends Component{
+import { Layout, notification } from 'antd';
+const { Content } = Layout;
+
+class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            loggedInStatus: "NOT_LOGGED_IN",
-            ciasteczko: Cookies.get('JSESSIONID'),
-            user: {}
+            currentUser: null,
+            isAuthenticated: false,
+            isLoading: false
         }
+        this.handleLogout = this.handleLogout.bind(this);
+        this.loadCurrentUser = this.loadCurrentUser.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
 
-        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        notification.config({
+            placement: 'topRight',
+            top: 70,
+            duration: 3,
+        });
     }
 
-    checkLoginStatus() {
-        
+    loadCurrentUser() {
+        this.setState({
+            isLoading: true
+        });
+        getCurrentUser()
+            .then(response => {
+                this.setState({
+                    currentUser: response,
+                    isAuthenticated: true,
+                    isLoading: false
+                });
+            }).catch(error => {
+            this.setState({
+                isLoading: false
+            });
+        });
     }
 
-    handleSuccessfulAuth(data) {
-        //update todo
+    componentDidMount() {
+        this.loadCurrentUser();
     }
 
-    render(){
-        return(
-            <Router>
+    // Handle Logout, Set currentUser and isAuthenticated state which will be passed to other components
+    handleLogout(redirectTo="/", notificationType="success", description="You're successfully logged out.") {
+        localStorage.removeItem(ACCESS_TOKEN);
 
-                <div id="page-wraper">
-                    <div id="logo" className="container">
-                        <h1><span className="icon icon-ambulance icon-size"></span><Link to="/">Medi<span>Shield</span></Link></h1>
-                    </div>
+        this.setState({
+            currentUser: null,
+            isAuthenticated: false
+        });
 
-                    <div id="wrapper" className="container">
-                        <Menu />
+        this.props.history.push(redirectTo);
+
+        notification[notificationType]({
+            message: 'Polling App',
+            description: description,
+        });
+    }
+
+    /*
+     This method is called by the Login component after successful login
+     so that we can load the logged-in user details and set the currentUser &
+     isAuthenticated state, which other components will use to render their JSX
+    */
+    handleLogin() {
+        notification.success({
+            message: 'Polling App',
+            description: "You're successfully logged in.",
+        });
+        this.loadCurrentUser();
+        this.props.history.push("/");
+    }
+
+    render() {
+        if(this.state.isLoading) {
+            return <LoadingIndicator />
+        }
+        return (
+            <Layout className="app-container">
+                <AppHeader isAuthenticated={this.state.isAuthenticated}
+                           currentUser={this.state.currentUser}
+                           onLogout={this.handleLogout} />
+
+                <Content className="app-content">
+                    <div className="container">
                         <Switch>
-                            <Route path="/register" component={Register}></Route>
-                            <Route path="/login" component={Login}></Route>
-                            <Route path="/" exact component={Home}></Route>
-                            <Route path="/faq" component={Faq}></Route>
+                            <Route path="/login" render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
+                            <Route path="/signup" component={Signup}></Route>
+                            <Route path="/users/:username"
+                                   render={(props) => <Profile isAuthenticated={this.state.isAuthenticated} currentUser={this.state.currentUser} {...props}  />}>
+                            </Route>
+                                <Route component={NotFound}></Route>
                         </Switch>
                     </div>
-
-                </div>
-                <Footer/>
-            </Router>
+                </Content>
+            </Layout>
         );
     }
 }
 
-export default App;
+export default withRouter(App);
--- medical-center/src/index.js
+++ medical-center/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { CookiesProvider } from "react-cookie";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <CookiesProvider>
     <App />
-  </React.StrictMode>,
+  </CookiesProvider>,
+
   document.getElementById('main-body')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
