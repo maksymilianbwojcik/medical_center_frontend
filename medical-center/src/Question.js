59e4224bc76f108f53f3edecc50d9c0eb4195196
Nikola Świerczek <nswierc@student.agh.edu.pl>
2020-07-04 17:21:19 +0200

    FAQ view

--- medical-center/src/Question.js
+++ medical-center/src/Question.js
@@ -3,14 +3,12 @@ import React, {Component} from 'react';
 class Question extends Component{
 
     render(props){
-        return (
-        <div id="faq-wraper">
-            <div id="page">
-                <div id="box1">
-                    <div className="title">
-                        <h2> {this.props.info.question} </h2>
+        return (       
+        <div id="page">
+            <div className="title">
+                <div id="question-wrapper">
+                        <div className="quest">{this.props.info.id+ ". "+ this.props.info.question} </div> 
                         <p className="answer"> {this.props.info.answer}</p>
-                    </div>
                 </div>
             </div>
         </div>
