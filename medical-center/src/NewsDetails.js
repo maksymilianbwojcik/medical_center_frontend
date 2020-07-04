b9fb602917ae8fd01616b7dd63e8e0b599268848
Khamzat <khamzat@localhost.localdomain>
2020-07-04 12:27:22 +0200

    bardzo wielki commit nie wystraszcie sie(NewDetails bardzo powazna poprawka)

--- medical-center/src/NewsDetails.js
+++ medical-center/src/NewsDetails.js
@@ -8,9 +8,11 @@ class NewsDetails extends Component{
                 <div id="page">
                     <div id="box1">
                         <div className="title">
-                            <h2> {this.info.title} </h2>
-                            <p className="date">{this.info.date}</p>
-                            <p className="text"> {this.info.text}</p>
+                            <h2> {this.props.info.title} </h2>
+
+                            <p className="date">{this.props.info.date}</p>
+                            <p className="text"> {this.props.info.text}</p>
+
                         </div>
                     </div>
                 </div>
